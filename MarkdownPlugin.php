fc609ceb55d9c11296da45bb77973047db19de6d
Chimo <chimo@chromic.org>
2016-01-12 17:58:38 +0000

    Add "render_text" method
    Make it clear what exacly is pulled from common_render_text

--- MarkdownPlugin.php
+++ MarkdownPlugin.php
@@ -8,21 +8,28 @@ class MarkdownPlugin extends Plugin
 {
     const VERSION = '0.0.4';
 
+    // From /lib/util.php::common_render_text
+    // We don't want to call it directly since we don't want to
+    // run common_linkify() on the text
+    function render_text($text)
+    {
+        $text = common_remove_unicode_formatting($text);
+
+        $text = preg_replace('/[\x{0}-\x{8}\x{b}-\x{c}\x{e}-\x{19}]/', '', $text);
+
+        // Link #hashtags
+        $rendered = preg_replace_callback('/(^|\&quot\;|\'|\(|\[|\{|\s+)#([\pL\pN_\-\.]{1,64})/u',
+            function ($m) { return "{$m[1]}#".common_tag_link($m[2]); }, $text);
+
+        return $rendered;
+    }
+
     function onStartNoticeSave($notice)
     {
         // Only run this on local notices
         if ($notice->isLocal()) {
-            $text = $notice->content;
 
-            // From /lib/util.php::common_render_text
-            // We don't want to call it directly since we don't want to
-            // run common_linkify() on the text
-            $text = common_remove_unicode_formatting($text);
-            $text = preg_replace('/[\x{0}-\x{8}\x{b}-\x{c}\x{e}-\x{19}]/', '', $text);
-
-            // Link #hashtags
-            $rendered = preg_replace_callback('/(^|\&quot\;|\'|\(|\[|\{|\s+)#([\pL\pN_\-\.]{1,64})/u',
-            function ($m) { return "{$m[1]}#".common_tag_link($m[2]); }, $text);
+            $rendered = $this->render_text($notice->content);
 
             // Some types of notices do not have the hasParent() method, but they're not notices we are interested in
             if (method_exists($notice, 'hasParent')) {
@@ -32,7 +39,6 @@ class MarkdownPlugin extends Plugin
 
             // Prevent leading #hashtags from becoming headers by adding a backslash
             // before the "#", telling markdown to leave it alone
-            // $repl_rendered = preg_replace('/^#[\pL\pN_\-\.]{1,64}/u', 'replaced!', $rendered);
             $repl_rendered = preg_replace('/^#<span class="tag">/u', '\\\\\\0', $rendered);
 
             // Only use the replaced value from above if it returned a success
