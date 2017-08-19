99b512ef0d8ab3fa8009b975aa25743f95ec8089
Chimo <chimo@chromic.org>
2017-08-19 03:28:02 +0000

    Revert "[WIP] Per-notice markdown setting"
    Remove unfinished feature from master branch
    
    This reverts commit e8c34e3fc00f62fb189f78fcf04c36da38b13586.

--- js/gs-markdown.js
+++
@@ -1,9 +0,0 @@
-( function() {
-	/*
-	 * TODO:
-	 *  Check the state of the checkbox (checked = markdown-enabled)
-	 *  Save the opposite setting
-	 *  Change the state of the button
-	 */
-}() );
-
